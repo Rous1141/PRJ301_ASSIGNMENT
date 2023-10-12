eb193bc693d76b638ee20b41978118a5b2909191
Rous1141 <132892586+Rous1141@users.noreply.github.com>
2023-10-12 13:10:00 +0700

    Restructure

--- src/java/Controller/CentralController.java
+++ src/java/Controller/CentralController.java
@@ -31,14 +31,16 @@ public class CentralController extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            String SignIn_Page = "signupPage.jsp";
-            String LogIn_Page = "loginPage.jsp";
-            String Shop_Page = "shopPage.jsp";
-            String Admin_Page = "adminPage.jsp";
-            String Cart_Page = "cartPage.jsp";
-            String History_Page = "historyPage.jsp";
-            String Shipper_Page = "shipperPage.jsp";
-            String Error_Page = "errorPage.jsp";
+            String SignIn_Page = "Customer/signupPage.jsp";
+            String LogIn_Page = "Components/loginPage.jsp";
+            String Shop_Page = "Customer/shopPage.jsp";
+            String Admin_Page = "Admin/adminPage.jsp";
+            String Cart_Page = "Customer/cartPage.jsp";
+            String History_Page = "Customer/historyPage.jsp";
+            String Shipper_Page = "Components/shipperPage.jsp";
+            String Error_Page = "Components/errorPage.jsp";
+            
+            
             
             /* TODO output your page here. You may use following sample code. */
             String action = request.getParameter("action");
