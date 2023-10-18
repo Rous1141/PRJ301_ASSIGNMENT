97d1a5eb1426bdf11b4d33045e6d6b0ddad832ce
thucnguyen1234 <147797581+thucnguyen1234@users.noreply.github.com>
2023-10-18 12:45:47 +0700

    Update CustomerDAO.java

--- src/java/DAO/CustomerDAO.java
+++ src/java/DAO/CustomerDAO.java
@@ -4,10 +4,60 @@
  */
 package DAO;
 
+import DTO.Customers;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import myLibrary.DatabaseConnection;
+
 /**
  *
  * @author hendrix
  */
 public class CustomerDAO {
-    
+
+    public static ArrayList<Customers> getAll() throws Exception {
+        Connection con;
+
+        ArrayList<Customers> result = new ArrayList<>();
+        try {
+            con = DatabaseConnection.makeConnection();
+            if (con != null) {
+                String sql = "SELECT [customer_id]\n"
+                        + "      ,[email]\n"
+                        + "      ,[name]\n"
+                        + "      ,[birth_date]\n"
+                        + "      ,[phone_number]\n"
+                        + "      ,[address]\n"
+                        + "      ,[status]\n"
+                        + "      ,[orders]\n"
+                        + "  FROM [dbo].[Customer]";
+                PreparedStatement pts = con.prepareStatement(sql);
+
+                ResultSet rs = pts.executeQuery();
+                while (rs != null && rs.next()) {
+                    int id = rs.getInt("customer_id");
+                    String mail = rs.getString("email");
+
+                    String name = rs.getString("name");
+                    Date birth = rs.getDate("birth_date");
+                    String phone = rs.getString("phone_number");
+                    String add = rs.getString("address");
+                    boolean staus = rs.getBoolean("status");
+                    String orders = rs.getString("orders");
+
+                    Customers emp = new Customers(id, name, birth, phone, add, mail, staus, orders);
+
+                    result.add(emp);
+                }
+                con.close();
+            }
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+
+        return result;
+    }
 }
