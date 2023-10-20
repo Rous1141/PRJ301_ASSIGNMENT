0eac2f74b9a35393f54a13e1b0fe8942c02d743c
thucnguyen1234 <147797581+thucnguyen1234@users.noreply.github.com>
2023-10-20 19:10:47 +0700

    update

--- src/java/myLibrary/DatabaseConnection.java
+++ src/java/myLibrary/DatabaseConnection.java
@@ -15,8 +15,8 @@ public class DatabaseConnection {
     public static Connection makeConnection() throws Exception{
         Connection cn=null;
 
-        String IP="10.211.55.2";   
-        String instanceName="sql1";
+        String IP="localhost";   
+        String instanceName="DESKTOP-7705AIH\\SQLEXPRESS";
 
         String port="1433";
         String uid="sa"; 
